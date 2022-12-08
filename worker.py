0618e9f260d8f1b0b10acbcf2f498611f4c6d155
lairdp <lairdp@users.noreply.github.com>
2022-12-08 11:54:07 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -16,14 +16,13 @@ def hello():
 
 @app.route("/add",methods=['GET','POST'])
 def add():
-  print(request)
   if request.method=='GET':
     return "Use post to add" # replace with form template
   else:
     token=get_api_key()
     with open('payload.json') as p:
       tdata=json.load(p)
-    tdata['name']='slave'+str(request.form['num'])
+    tdata['name']='slave3' #testing +str(request.form['num'])
     data=json.dumps(tdata)
     url='https://www.googleapis.com/compute/v1/projects/spark-371009/zones/europe-west1-b/instances'
     headers={"Authorization": "Bearer "+token}
