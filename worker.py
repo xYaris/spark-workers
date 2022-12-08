73627a9955fa3a00ab094f308265cfd727a3e94b
lairdp <lairdp@users.noreply.github.com>
2022-12-08 12:14:08 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 
 def get_api_key() -> str:
-    secret = os.environ.get("compute-api-key")
+    secret = os.environ.get("COMPUTE_API_KEY")
     if secret:
         return secret
       
